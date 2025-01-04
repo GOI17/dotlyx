4e3cc1750b4e2e70910f73676ca5d066395d8efa
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-01-04 01:30:43 -0800

    fix: update flake

--- scripts/nix/build_config.sh
+++ scripts/nix/build_config.sh
@@ -6,13 +6,14 @@ command_exists() {
     type "$1" >/dev/null 2>&1
 }
 
+cd "$HOME/.config/nix-darwin"
+
 if ! command_exists darwin-rebuild; then
   echo "DOTLYX: Installing nix-darwin..."
   nix --extra-experimental-features "nix-command flakes" \
-	  run nix-darwin -- switch --flake "$HOME/.config/nix-darwin"
+	  run nix-darwin -- switch
   /bin/zsh -c "source '$HOME/.zshrc'"
 fi
 
 echo "DOTLYX: Building nix configurations..."
-
-darwin-rebuild switch --flake "$HOME/.config/nix-darwin"
+darwin-rebuild switch
