0694eefa572a3aaa4d0c5e5244c21b30f2a9f97a
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-01-07 14:13:14 -0800

    feature: move zsh configs to flake

--- dotfiles_template/modules/nix/flake.nix
+++ dotfiles_template/modules/nix/flake.nix
@@ -102,7 +102,7 @@
 	zsh-syntax-highlighting
 	zsh-fast-syntax-highlighting
 	nix-zsh-completions
-	(import = import ./nix-zimfw.nix { inherit pkgs; })
+	(import ./nix-zimfw.nix { inherit pkgs; })
       ];
 
       # Necessary for using flakes on this system.
