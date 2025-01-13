603fb2644ef12292097bbd07c8ec01835a27ee34
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-01-13 11:57:30 -0800

    fix: update nix-darwin installation

--- scripts/nix/build_config.sh
+++ scripts/nix/build_config.sh
@@ -9,11 +9,16 @@ command_exists() {
 cd "$HOME/.config/nix-darwin"
 
 if ! command_exists darwin-rebuild; then
-  echo "DOTLYX: Installing nix-darwin..."
-  nix --extra-experimental-features "nix-command flakes" \
-	run nix-darwin -- switch \
-	--flake .
-  #/bin/zsh -c "source '$HOME/.zshrc'"
+	echo "DOTLYX: Installing nix-darwin..."
+	sudo mkdir -p /etc/nix
+	sudo touch /etc/nix/registry.json
+	sudo chmod 644 /etc/nix/registry.json
+	export SSL_CERT_FILE=/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt
+	export NIX_SSL_CERT_FILE=/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt
+	nix --extra-experimental-features "nix-command flakes" \
+		run nix-darwin -- switch \
+		--flake .
+	#/bin/zsh -c "source '$HOME/.zshrc'"
 fi
 
 echo "DOTLYX: Setting your nix configurations..."
