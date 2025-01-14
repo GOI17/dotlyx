a3d7ee91d81b17f92ce79d8c5408ba14f2cc1d5c
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-01-14 14:19:54 -0800

    feature: add home manager

---
+++ dotfiles_template/modules/nix/home-manager.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+{
+	homeconfig = {pkgs, ...}: {
+		# this is internal compatibility configuration 
+		# for home-manager, don't change this!
+		home.stateVersion = "23.05";
+		# Let home-manager install and manage itself.
+		programs.home-manager.enable = true;
+
+		home.packages = with pkgs; [];
+
+		home.sessionVariables = {
+			EDITOR = "vim";
+		};
+	};
+
+	home-manager.darwinModules.home-manager  {
+		home-manager.useGlobalPkgs = true;
+		home-manager.useUserPackages = true;
+		home-manager.verbose = true;
+		home-manager.users.$USER = homeconfig;
+	}
+}
