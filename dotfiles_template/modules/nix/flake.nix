ac568f4663ed3960a5399a6509a227bcd1bd3ba1
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-01-14 17:52:14 -0800

    feature: add home manager

--- dotfiles_template/modules/nix/flake.nix
+++ dotfiles_template/modules/nix/flake.nix
@@ -37,9 +37,22 @@
     ...
   }:
   let
-    #systemDefaults = import ./macos_defaults.nix;
-    hostName="XXX_USER_HOSTNAME_XXX";
-    userName="$(whoami)";
+	#systemDefaults = import ./macos_defaults.nix;
+	hostName="XXX_USER_HOSTNAME_XXX";
+	userName="$(whoami)";
+	homeconfig = {pkgs, ...}: {
+		# this is internal compatibility configuration 
+		# for home-manager, don't change this!
+		home.stateVersion = "23.05";
+		# Let home-manager install and manage itself.
+		programs.home-manager.enable = true;
+
+		home.packages = with pkgs; [];
+
+		home.sessionVariables = {
+			EDITOR = "vim";
+		};
+	};
   in
   {
     darwinConfigurations."${hostName}" = nix-darwin.lib.darwinSystem {
@@ -47,7 +60,7 @@
 	      ./nix-darwin.nix { inherit inputs; inherit userName; }
 	      #./home-manager.nix { inherit userName; }
 
-		home-manager.darwinModules.home-manager = {
+		home-manager.darwinModules.home-manager {
 			home-manager.useGlobalPkgs = true;
 			home-manager.useUserPackages = true;
 			home-manager.verbose = true;
