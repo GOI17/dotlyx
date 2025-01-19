c79a2aefa70d6d0d8bacde812b8ee8dcd4066a31
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-01-19 02:09:27 -0800

    feature: homemanager

--- dotfiles_template/modules/nix/flake.nix
+++ dotfiles_template/modules/nix/flake.nix
@@ -98,7 +98,7 @@
 					};
 				}
 				home-manager.darwinModules.home-manager {
-					home-manager = home-manager-config { inherit user lib; };
+					home-manager = home-manager-config { inherit user (inherit nixpkgs) lib; };
 				} 
 				nix-homebrew.darwinModules.nix-homebrew {
 					nix-homebrew = homebrew-config.module {
