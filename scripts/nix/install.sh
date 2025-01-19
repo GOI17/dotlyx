d9160b525f03c6d8c1e0a62ec835a8149d3edd0e
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-01-19 01:19:12 -0800

    feature: homemanager

--- scripts/nix/install.sh
+++ scripts/nix/install.sh
@@ -79,7 +79,7 @@ source "$DOTLYX_HOME_PATH/scripts/nix/check_for_required_tools.sh"
 echo "DOTLYX: Installing dotlyx submodules..."
 git submodule update --init --recursive
 cp -r "$DOTLYX_HOME_PATH/dotfiles_template/"* .
-sed -i -e "s|XXX_USER_DOTFILES_PATH_XXX|$USER_DOTFILES_PATH|g" "./modules/nix/modules/system/environment.nix"
+sed -i -e "s|XXX_USER_DOTFILES_PATH_XXX|$USER_DOTFILES_PATH|g" "./modules/nix/modules/system/environment/environment.nix"
 for symlinks_file in "conf.yaml" "conf.macos.yaml"; do
 	"$DOTLYX_HOME_PATH/modules/dotbot/bin/dotbot" -d "$DOTLYX_HOME_PATH" -c "./symlinks/$symlinks_file"
 done
