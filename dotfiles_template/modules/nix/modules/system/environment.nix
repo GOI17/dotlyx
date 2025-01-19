bd60cfe9b1100ac5cc052da47c065a70a16952d4
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-01-18 16:26:54 -0800

    feature: homemanager

--- dotfiles_template/modules/nix/modules/system/environment.nix
+++ dotfiles_template/modules/nix/modules/system/environment.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }: 
 
 {
-	environment.extraInit = import ../modules/shell/functions.nix;
+	environment.extraInit = import ../shell/functions.nix;
 
-	environment.shellAliases = import ../modules/shell/aliases.nix;
+	environment.shellAliases = import ../shell/aliases.nix;
 
-	environment.variables = import ../modules/shell/exports.nix
+	environment.variables = import ../shell/exports.nix
 	// rec {
 		USER_DOTFILES_PATH= "XXX_USER_DOTFILES_PATH_XXX";
 		DOTLYX_HOME_PATH = "${USER_DOTFILES_PATH}/modules/dotlyx";
