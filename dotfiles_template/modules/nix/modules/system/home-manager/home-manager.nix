9702737a77f853ba4a334c87c1202b7d9dc3f795
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-01-19 18:41:36 -0800

    feature: homemanager

--- dotfiles_template/modules/nix/modules/system/home-manager/home-manager.nix
+++ dotfiles_template/modules/nix/modules/system/home-manager/home-manager.nix
@@ -19,7 +19,7 @@ let
 		programs.zsh = import ../../programs/zsh.nix;
 		home.file = {
 			#".config/nvim-nvchad" = "$HOME/.dotfiles/editors/nvim-nvchad";
-			".zshrc".source = "$HOME/.dotfiles/shell/zsh/.zshrc";
+			".zshrc".source = import "$HOME/.dotfiles/shell/zsh/.zshrc";
 			".zshrc".force = true;
 			#".zshenv".source = "$HOME/.dotfiles/shell/zsh/.zshenv";
 			#".zlogin".source = "$HOME/.dotfiles/shell/zsh/.zlogin";
