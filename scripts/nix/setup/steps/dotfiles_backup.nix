bd8add2b34fc80135226af8f643bcaf69933b3a7
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-02-23 23:13:39 -0600

    feature: create entry point to install

--- scripts/nix/setup/steps/dotfiles_backup.nix
+++ scripts/nix/setup/steps/dotfiles_backup.nix
@@ -76,7 +76,7 @@
    
     git init
     git config --global protocol.file.allow always
-    git submodule add -b main ~/dotlyx $DOTLYX_HOME_PATH
+    git submodule add -b main ~/dotlyx modules/dotlyx
     git submodule update --init --recursive
     git config --global protocol.file.allow never
 
