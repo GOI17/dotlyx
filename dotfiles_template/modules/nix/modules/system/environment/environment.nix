2619f0bb461c4738eb9ba72cb426f89084a14038
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-03-05 21:34:09 -0600

    feature: update shell routes

--- dotfiles_template/modules/nix/modules/system/environment/environment.nix
+++ dotfiles_template/modules/nix/modules/system/environment/environment.nix
@@ -3,11 +3,11 @@
 with lib;
 
 {
-	environment.extraInit = import ../../shell/functions.nix;
+	environment.extraInit = import ../../../../../shell/functions.nix;
 
-	environment.shellAliases = import ../../shell/aliases.nix;
+	environment.shellAliases = import ../../../../../shell/aliases.nix;
 
-	environment.variables = import ../../shell/exports.nix
+	environment.variables = import ../../../../../shell/exports.nix
 	// rec {
 		USER_DOTFILES_PATH= "XXX_USER_DOTFILES_PATH_XXX";
 		DOTLYX_HOME_PATH = "${USER_DOTFILES_PATH}/modules/dotlyx";
