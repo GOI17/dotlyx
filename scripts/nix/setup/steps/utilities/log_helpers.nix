99ccc434bef4b6d322875ed0b6a791dfc09bac61
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-03-06 21:20:51 -0600

    feature: clear existant dotlyx folder

--- scripts/nix/setup/steps/utilities/log_helpers.nix
+++ scripts/nix/setup/steps/utilities/log_helpers.nix
@@ -7,13 +7,13 @@ rec {
   _colorized = { message, color }: ''
     echo "${color}${message}"
   '';
-  _s = { message }: ''
-    echo "\${_colorized "DOTLYX:" GREEN} ${message}"
+  _s = message: ''
+    echo "\${_colorized { color = ${GREEN}; message = "DOTLYX:"; } } ${message}"
   '';
-  _e = { message }: ''
-    echo "\${_colorized "DOTLYX:" RED} ${message}"
+  _e = message: ''
+    echo "\${_colorized { color = ${RED}; message = "DOTLYX:"; } } ${message}"
   '';
-  _w = { message }: ''
-    echo "\${_colorized "DOTLYX:" YELLOW} ${message}"
+  _w = message: ''
+    echo "\${_colorized { color = ${YELLOW}; message = "DOTLYX:"; } } ${message}"
   '';
 }
