fdcf3423134ad6794ae3c475389b72f82e4234f7
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-03-06 21:38:54 -0600

    feature: fix log helpers

--- scripts/nix/setup/steps/utilities/log_helpers.nix
+++ scripts/nix/setup/steps/utilities/log_helpers.nix
@@ -2,12 +2,12 @@ with import ./colors.nix;
 
 rec {
   _s = message: ''
-    echo -e "\${GREEN}DOTLYX:\${RESET} \${WHITE}\${message}\${RESET}"
+    echo -e "\${GREEN}DOTLYX:\${RESET} \${WHITE}${message}\${RESET}"
   '';
   _e = message: ''
-    echo -e "\${RED}DOTLYX:\${RESET} \${WHITE}\${message}\${RESET}"
+    echo -e "\${RED}DOTLYX:\${RESET} \${WHITE}${message}\${RESET}"
   '';
   _w = message: ''
-    echo -e "\${YELLOW}DOTLYX:\${RESET} \${WHITE}\${message}\${RESET}"
+    echo -e "\${YELLOW}DOTLYX:\${RESET} \${WHITE}${message}\${RESET}"
   '';
 }
