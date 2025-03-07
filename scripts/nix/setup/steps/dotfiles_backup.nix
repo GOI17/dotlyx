9732aa607aadc93423e3cf39efa70e81f5678339
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-03-06 21:08:57 -0600

    feature: remove dotbot

--- scripts/nix/setup/steps/dotfiles_backup.nix
+++ scripts/nix/setup/steps/dotfiles_backup.nix
@@ -1,17 +1,17 @@
-{
-  script = with import ./utilities/colors.nix; ''
-    get_local_time_in_seconds ()
-    {
-      date +%s;
-    }
+with import ./utilities/colors.nix; 
+with import ./utilities/log_helpers.nix;
 
-    generate_backup_dir () {
-      local backup_path="pre_dotlyx_dotfiles.$(get_local_time_in_seconds).back"
-      echo "DOTLYX: Creating backup in '$backup_path'";
+rec {
+  get_local_time_in_seconds = ''
+      date +%s;
+  '';
+  create_backup_dir = ''
+      local backup_path="pre_dotlyx_dotfiles.${get_local_time_in_seconds}.back"
+      ${_s "Creating backup in '$backup_path'"}
       local user_dotfiles_absolute_path=''$(realpath $USER_DOTFILES_PATH)
       local parent_user_dotfiles_path=''$(dirname $user_dotfiles_absolute_path)
       mv ''$user_dotfiles_absolute_path "''$parent_user_dotfiles_path/''$backup_path";
-      local home_user_dotfiles_path="pre_dotlyx_home_dotfiles.''$(get_local_time_in_seconds).back"
+      local home_user_dotfiles_path="pre_dotlyx_home_dotfiles.${get_local_time_in_seconds}.back"
       mkdir -pv "''$parent_user_dotfiles_path/''$backup_path/''$home_user_dotfiles_path"
       local current_dotfiles="''$(ls -a ~ | grep -v dotlyx | grep -e zsh -e bash -e dot)"
       local files_with_errors=()
@@ -20,60 +20,62 @@
         [ ''$? -ne 0 ] && files_with_errors+=(''$filename)
       done
       if [ ! -z ''$files_with_errors ]; then
-        echo "There were some issues moving these files ''${files_with_errors[*]}
+        ${_e "There were some issues moving these files $files_with_errors[*]
         Posible solutions:
         - Run the install process again.
         - Verify if those files are in the same path.
-        "
+        "}
         exit 1
       fi
-    }
-
+  '';
+  script = ''
     if ! [[ ''$SHELL =~ "zsh" ]]; then
-        echo "DOTLYX: Setting zsh as default shell"
+        ${_s "Setting zsh as default shell"}
         sudo chsh -s "''$(command -v zsh)"
     fi
 
-    echo "DOTLYX: Looking for existing dotfiles..."
+    ${_s "Looking for existing dotfiles..."}
 
     if [ -d "''$USER_DOTFILES_PATH" ]; then
       while true; do
         read -p "The path ''${USER_DOTFILES_PATH} already exists. Do you want to create a backup? (y/n): " is_backup_required
         case ''$is_backup_required in
           [Yy] ) 
-            generate_backup_dir
+            ${create_backup_dir}
             break
             ;;
           [Nn] )
-            echo "DOTLYX: Skipping backup";
-	    rm -rf ''$USER_DOTFILES_PATH
+            ${_w "Skipping backup"}
+            rm -rf ''$USER_DOTFILES_PATH
             break
             ;;
           *)
-            echo "DOTLYX: Plase provide a valid option."
+            ${_e "Plase provide a valid option."}
             ;;
         esac
       done
     fi
 
-    echo "DOTLYX: dotfiles will be located in: ''$USER_DOTFILES_PATH"
+    ${_s "dotfiles will be located in: $USER_DOTFILES_PATH"}
     mkdir -pv "''$USER_DOTFILES_PATH" 2>&1
 
     export USER_DOTFILES_PATH="''$(realpath $USER_DOTFILES_PATH)"
     if [ -z ''$USER_DOTFILES_PATH ]; then
-      echo "DOTLYX: There was an issue setting USER_DOTFILES_PATH variable, please try again";
-      exit 1;
+      ${_e "There was an issue setting USER_DOTFILES_PATH variable, please try again"}
+      exit 1
     fi
 
     cd ''$USER_DOTFILES_PATH
 
     # TODO: Move into his own step script 'Manage dotlyx settings'
+    ${_s "Setting dotlyx core configurations"}
+
     export DOTLYX_HOME_PATH="''$USER_DOTFILES_PATH/modules/dotlyx"
     if [ -z ''$DOTLYX_HOME_PATH ]; then
-      echo "DOTLYX: There was an issue setting DOTLYX_HOME_PATH variable, please try again";
-      exit 1;
+      ${_e "There was an issue setting DOTLYX_HOME_PATH variable, please try again"}
+      exit 1
     fi
-   
+
     git init
     git config --global protocol.file.allow always
     git submodule add -b main ''$HOME/dotlyx modules/dotlyx
@@ -95,33 +97,25 @@
     cp -r "''$DOTLYX_HOME_PATH/dotfiles_template/"* .
     sed -i -e "s|XXX_USER_DOTFILES_PATH_XXX|''$USER_DOTFILES_PATH|g" "./modules/nix/flake.nix"
     sed -i -e "s|XXX_USERNAME_XXX|''$(whoami)|g" "./modules/nix/flake.nix"
-    for symlinks_file in "conf.yaml" "conf.macos.yaml"; do
-     	"''$DOTLYX_HOME_PATH/modules/dotbot/bin/dotbot" -d "''$DOTLYX_HOME_PATH" -c "./symlinks/$symlinks_file"
-    done
-
-    echo "DOTLYX: Initializing Nix default configurations"
-
-    command_exists() {
-        type "''$1" >/dev/null 2>&1
-    }
+    cp ./dotfiles_template/modules/nix/flake.nix ''$HOME/.config/nix-darwin/flake.nix
 
     cd "''$HOME/.config/nix-darwin"
 
-    if ! command_exists darwin-rebuild; then
-        echo "DOTLYX: Installing nix-darwin..."
-	nix --extra-experimental-features "nix-command flakes" \
-		run nix-darwin -- switch \
-		--flake .#dotlyx --impure
+    if ! ${command_exists darwin-rebuild}; then
+      ${_s "Installing nix-darwin..."}
+      nix --extra-experimental-features "nix-command flakes" \
+        run nix-darwin -- switch \
+        --flake .#dotlyx --impure
     else
-	darwin-rebuild switch --flake .#dotlyx --impure
+      darwin-rebuild switch --flake .#dotlyx --impure
     fi
 
     if [ ''$? -ne 0 ]; then
-        echo "DOTLYX: We stopped the installation. Try with a new installation process"
+        ${_e "We stopped the installation. Try with a new installation process"}
         exit 1
     fi
 
     cd ''$HOME
-    echo "DOTLYX: Restart your terminal and Welcome to Dotlyx!"
+    ${_s "Restart your terminal and Welcome to Dotlyx!"}
   '';
 }
---
+++ scripts/nix/setup/steps/utilities/log_helpers.nix
@@ -0,0 +1,19 @@
+with import ./colors.nix;
+
+rec {
+  command_exists = ''
+    type $1 >/dev/null 2>&1
+  '';
+  _colorized = { message; color; }: ''
+    echo "${color}${message}"
+  '';
+  _s = { message; }: ''
+    echo "\${_colorized "DOTLYX:" GREEN} ${message}"
+  '';
+  _e = { message; }: ''
+    echo "\${_colorized "DOTLYX:" RED} ${message}"
+  '';
+  _w = { message; }: ''
+    echo "\${_colorized "DOTLYX:" YELLOW} ${message}"
+  '';
+}
