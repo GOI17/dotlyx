c96cf69e50c4b22b3f87f1c6f6abb33bcf35862b
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-03-06 22:45:26 -0600

    feature: fix copy flake into .config

--- scripts/nix/setup/steps/dotfiles_backup.nix
+++ scripts/nix/setup/steps/dotfiles_backup.nix
@@ -97,16 +97,14 @@ rec {
     cp -r "''$DOTLYX_HOME_PATH/dotfiles_template/"* .
     sed -i -e "s|XXX_USER_DOTFILES_PATH_XXX|''$USER_DOTFILES_PATH|g" "./modules/nix/flake.nix"
     sed -i -e "s|XXX_USERNAME_XXX|''$(whoami)|g" "./modules/nix/flake.nix"
-    ln -sf ./modules/nix/flake.nix ''$HOME/.config/nix-darwin/flake.nix 
-    cd ''$HOME/.config/nix-darwin
 
     if ! $(type darwin-rebuild >/dev/null 2>&1); then
       ${_s "Installing nix-darwin..."}
       nix --extra-experimental-features "nix-command flakes" \
         run nix-darwin -- switch \
-        --flake .#dotlyx --impure
+        --flake ./modules/nix/flake.nix#dotlyx --impure
     else
-      darwin-rebuild switch --flake .#dotlyx --impure
+      darwin-rebuild switch --flake ./modules/nix/flake.nix#dotlyx --impure
     fi
 
     if [ ''$? -ne 0 ]; then
