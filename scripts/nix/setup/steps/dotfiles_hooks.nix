e43a60afd1622e2b01627bf839ce2ffa3d25ecc4
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-03-09 21:55:03 -0600

    feature: auto rebuild on flake change

--- scripts/nix/setup/steps/dotfiles_hooks.nix
+++ scripts/nix/setup/steps/dotfiles_hooks.nix
@@ -1,10 +1,8 @@
-{ pkgs ? import <nixpkgs> {} }:
-
 {
   script = ''
     #!/usr/bin/env bash
 
-    echo "darwin-rebuild switch --flake ${pkgs.dotlyx-setup}/.config/nix-darwin#dotlyx --impure" >> ''$USER_DOTFILES_PATH/.git/hooks/post-commit
+    echo "darwin-rebuild switch --flake ''$HOME/.config/nix-darwin#dotlyx --impure" >> ''$USER_DOTFILES_PATH/.git/hooks/post-commit
 
     chmod +x ''$USER_DOTFILES_PATH/.git/hooks/post-commit
   '';
