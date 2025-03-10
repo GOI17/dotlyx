61337082cd1e1a50aa1e34ec611e6b36d9d6bfe5
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-03-09 20:27:10 -0600

    fix: update imports

--- dotfiles_template/system/environment.nix
+++ dotfiles_template/system/environment.nix
@@ -6,11 +6,11 @@ with import ../env.nix;
 {
 	environment.extraInit = import ../shell/functions.nix;
 	environment.shellAliases = import ../shell/aliases.nix;
-	environment.variables = import ../shell/exports.nix;
-	environment.pathsToLink = [ "/share/zsh" ];
-	environment.systemPackages = systemPackages;
+	environment.variables = import ../shell/exports.nix
   // {
     testing = ${dotfilesDirectory};
   };
+	environment.pathsToLink = [ "/share/zsh" ];
+	environment.systemPackages = systemPackages;
 }
 
