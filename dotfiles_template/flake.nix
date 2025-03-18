cb2cdf5bad93298ad655810cdce5cfbe669cb9c4
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-03-18 02:33:30 -0600

    feature: add multiplatform support

--- dotfiles_template/flake.nix
+++ dotfiles_template/flake.nix
@@ -77,7 +77,7 @@
   in
   {
     darwinConfigurations."dotlyx" = nix-darwin.lib.darwinSystem {
-      modules = [
+      modules = commonModules ++ import ./os/default.nix { inherit mac-app-util; inherit self; } ++ [
         home-manager.darwinModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
