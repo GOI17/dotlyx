9879de0c1ec97fd7f3f802107d2b3ce1781af7d8
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-03-18 02:01:05 -0600

    feature: add multiplatform support

--- dotfiles_template/os/default.nix
+++ dotfiles_template/os/default.nix
@@ -3,21 +3,19 @@
 with stdenv;
 
 let
-    darwinCfg =
-      (if stdenv.isDarwin then with ./env.nix; [
-        mac-app-util.darwinModules.default
-        # Set Git commit hash for darwin-version.
-        {
-          system.configurationRevision = self.rev or self.dirtyRev or null;
-          home-manager.users."${user}" = import ./os/mac/silicon/home.nix;
-          nixpkgs.hostPlatform = "aarch64-darwin";
-        }
-      ] else []);
-    linuxCfg =
-      (if stdenv.isLinux then with ./env.nix; [
-        {
-            nixpkgs.hostPlatform = "x86_64-linux";
-            home-manager.users."${user}" = import ./os/linux/home.nix;
-        }
-      ] else []);
-in isDarwin then darwinCfg else linuxCfg;
+    darwinCfg = [
+      mac-app-util.darwinModules.default
+      # Set Git commit hash for darwin-version.
+      {
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+        home-manager.users."${user}" = import ./os/mac/silicon/home.nix;
+        nixpkgs.hostPlatform = "aarch64-darwin";
+      }
+    ];
+    linuxCfg = [
+      {
+        nixpkgs.hostPlatform = "x86_64-linux";
+        home-manager.users."${user}" = import ./os/linux/home.nix;
+      }
+    ];
+in if isDarwin then darwinCfg else linuxCfg;
