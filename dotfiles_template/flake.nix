03336afad53201555edb6a66fb4ff9642b6ba18f
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-03-18 01:58:33 -0600

    feature: add multiplatform support

--- dotfiles_template/flake.nix
+++ dotfiles_template/flake.nix
@@ -78,27 +78,11 @@
         ];
       }
     ];
-    darwinCfg = { stdenv, ... }:
-      (if stdenv.isDarwin then with ./env.nix; [
-        mac-app-util.darwinModules.default
-        # Set Git commit hash for darwin-version.
-        {
-          system.configurationRevision = self.rev or self.dirtyRev or null;
-          home-manager.users."${user}" = import ./os/mac/silicon/home.nix;
-          nixpkgs.hostPlatform = "aarch64-darwin";
-        }
-      ] else []);
-    linuxCfg = { stdenv, ... }: 
-      (if stdenv.isLinux then with ./env.nix; [
-        {
-            nixpkgs.hostPlatform = "x86_64-linux";
-            home-manager.users."${user}" = import ./os/linux/home.nix;
-        }
-      ] else []);
   in
   {
     darwinConfigurations."dotlyx" = nix-darwin.lib.darwinSystem {
-      modules = commonModules ++ darwinCfg { inherit self.stdenv; } ++ linuxCfg { inherit self.stdenv; } ++ [
+      modules = commonModules ++ [
+        import ./os
         home-manager.darwinModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
