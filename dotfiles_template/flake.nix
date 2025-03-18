d0c07dee7bfecce1802fd292248df6cec6b756f6
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-03-18 00:38:25 -0600

    feature: add multiplatform support

--- dotfiles_template/flake.nix
+++ dotfiles_template/flake.nix
@@ -37,67 +37,65 @@
 		home-manager,
 		...
 	}:
-  flake-utils.lib.eachDefaultSystem (system: 
-    let
-      modules = [
-        import ./system/environment.nix
-        {
-          nix.settings.experimental-features = "nix-command flakes";
-          fonts.fontconfig.enable = true;
-          # Used for backwards compatibility, please read the changelog before changing.
-          # $ darwin-rebuild changelog
-          system.stateVersion = 5;
-          # The platform the configuration will be used on.
-          # nixpkgs.hostPlatform = "aarch64-darwin";
-          # Allows to install non-opensource applications
-          nixpkgs.config.allowUnfree = true;
-          # Allows to install non-compatible architecture applications
-          nixpkgs.config.allowUnsupportedSystem = true;
-        }
-      ];
-      packages = { pkgs, ... }: with pkgs; [
-        # text editors
-        neovim
-        # terminal tools
-        mas
-        tree
-        wget
-        jq
-        gh
-        ripgrep
-        rename
-        neofetch
-        jump
-        gcc
-        openssl
-        asdf-vm
-        lazygit
-        eza
-        fd
-        fzf
-        zsh
-        nerd-fonts.caskaydia-cove
-        jetbrains-mono
-      ];
-    in
-    {
-      homeConfigurations = {
-        "dotlyx@linux" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          modules = modules ++ packages ++ [];
-        };
-        "dotlyx@mac-intel" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-darwin;
-          modules = modules ++ packages ++ [];
-        };
-        "dotlyx@mac-sillicon" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-          modules = modules ++ packages ++ [
-            import ./os/mac/silicon/home.nix
-            mac-app-util.darwinModules.default
-          ];
-        };
+  let
+    modules = [
+      import ./system/environment.nix
+      {
+        nix.settings.experimental-features = "nix-command flakes";
+        fonts.fontconfig.enable = true;
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 5;
+        # The platform the configuration will be used on.
+        # nixpkgs.hostPlatform = "aarch64-darwin";
+        # Allows to install non-opensource applications
+        nixpkgs.config.allowUnfree = true;
+        # Allows to install non-compatible architecture applications
+        nixpkgs.config.allowUnsupportedSystem = true;
+      }
+    ];
+    packages = { pkgs, ... }: with pkgs; [
+      # text editors
+      neovim
+      # terminal tools
+      mas
+      tree
+      wget
+      jq
+      gh
+      ripgrep
+      rename
+      neofetch
+      jump
+      gcc
+      openssl
+      asdf-vm
+      lazygit
+      eza
+      fd
+      fzf
+      zsh
+      nerd-fonts.caskaydia-cove
+      jetbrains-mono
+    ];
+  in
+  {
+    homeConfigurations = {
+      "dotlyx@linux" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = modules ++ packages ++ [];
+      };
+      "dotlyx@mac-intel" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+        modules = modules ++ packages ++ [];
+      };
+      "dotlyx@mac-sillicon" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+        modules = modules ++ packages ++ [
+          import ./os/mac/silicon/home.nix
+          mac-app-util.darwinModules.default
+        ];
       };
     };
-  );
+  };
 }
