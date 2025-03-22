725b3d5e27762f989ad40010ec96c11cb889abf8
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-03-21 23:57:35 -0600

    fix: flake

--- core/setup/install.nix
+++ core/setup/install.nix
@@ -82,7 +82,6 @@ in stdenv.mkDerivation {
   ];
   phases = [ "installPhase" ];
   installPhase = ''
-    export NIX_DEBUG=7
     mkdir -p $out/bin
     cp ${script}/bin/${name} $out/bin/${name}
   '';
