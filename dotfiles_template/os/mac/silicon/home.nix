71e9d73f0b21abf524332e8ff947e5077c449e1f
Jose Gilberto Olivas Ibarra <josegilbertoolivasibarra@Joses-MacBook-Pro.local>
2025-03-22 00:51:11 -0600

    fix: flake

--- dotfiles_template/os/mac/silicon/home.nix
+++ dotfiles_template/os/mac/silicon/home.nix
@@ -7,18 +7,6 @@ with import ../../../env.nix;
     # UI apps
     raycast
   ];
-  homebrew = { casks, masApps, ... }: {
-    enable = true;
-    casks = [
-      "docker"
-    ];
-    masApps = {};
-    onActivation = {
-      cleanup = "zap";
-      autoUpdate = true;
-      upgrade = true;
-    };
-  };
   home.homeDirectory = home;
   # this is internal compatibility configuration 
   # for home-manager, don't change this!
@@ -34,21 +22,5 @@ with import ../../../env.nix;
     #ex.
     # ".config/nvim-nvchad".source = "${dotfilesDirectory}/editors/nvim-nvchad";
     # ".config/nvim-nvchad".force = true;
-  };
-  nix-homebrew.darwinModules.nix-homebrew {
-    nix-homebrew = {
-      module = { taps, ... }: {
-        autoMigrate = true;
-        enable = true;
-        enableRosetta = true;
-        mutableTaps = false;
-        inherit user;
-        taps = {
-          "homebrew/homebrew-core" = nix-homebrew-core;
-          "homebrew/homebrew-cask" = nix-homebrew-cask;
-          "homebrew/homebrew-bundle" = nix-homebrew-bundle;
-        };
-      };
-    };
   };
 }
--- dotfiles_template/os/selector.nix
+++ dotfiles_template/os/selector.nix
@@ -1,4 +1,4 @@
-{ mac-app-util, self, pkgs, ... }:
+{ mac-app-util, nix-homebrew, nix-homebrew-core, nix-homebrew-cask, nix-homebrew-bundle, self, pkgs, ... }:
 
 with import ../env.nix;
 
@@ -6,11 +6,39 @@ let
   isDarwin = if builtins.currentSystem == "aarch64-darwin" then true else false;
   darwinCfg = [
     mac-app-util.darwinModules.default
+    nix-homebrew.darwinModules.nix-homebrew {
+      nix-homebrew = {
+        module = { taps, ... }: {
+          autoMigrate = true;
+          enable = true;
+          enableRosetta = true;
+          mutableTaps = false;
+          inherit user;
+          taps = {
+            "homebrew/homebrew-core" = nix-homebrew-core;
+            "homebrew/homebrew-cask" = nix-homebrew-cask;
+            "homebrew/homebrew-bundle" = nix-homebrew-bundle;
+          };
+        };
+      };
+    }
     # Set Git commit hash for darwin-version.
     {
       system.configurationRevision = self.rev or self.dirtyRev or null;
       home-manager.users."${user}" = import ./mac/silicon/home.nix { inherit pkgs; };
       nixpkgs.hostPlatform = "aarch64-darwin";
+      homebrew = {
+        enable = true;
+        casks = [
+          "docker"
+        ];
+        masApps = {};
+        onActivation = {
+          cleanup = "zap";
+          autoUpdate = true;
+          upgrade = true;
+        };
+      };
     }
   ];
   linuxCfg = [
