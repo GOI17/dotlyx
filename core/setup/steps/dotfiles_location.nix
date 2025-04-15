22c5cdf77f1fb3c1a4d71153710082a463d9c2e3
Gilberto Olivas <olivasgilberto@gmail.com>
2025-04-14 19:21:27 -0600

    fix: bad message placement

--- core/setup/steps/dotfiles_location.nix
+++ core/setup/steps/dotfiles_location.nix
@@ -1,16 +1,16 @@
 rec {
   defaultUserDotfilesPath="~/.dotfiles";
   messages = with import ./utilities/colors.nix; {
-	new = ''
-	    \${BOLD}\${WHITE}Where do you want to be located your dotfiles?
-	    \${MAGENTA}-\${RESET} \${BOLD}\${WHITE}Default: ${defaultUserDotfilesPath}
-	    \${MAGENTA}-\${RESET} \${BOLD}\${WHITE}Custom: ~/Documents/dotfiles
+    new = ''
+      \${BOLD}\${WHITE}Where do you want to be located your dotfiles?
+      \${MAGENTA}-\${RESET} \${BOLD}\${WHITE}Default: ${defaultUserDotfilesPath}
+      \${MAGENTA}-\${RESET} \${BOLD}\${WHITE}Custom: ~/Documents/dotfiles
 
-	    Press enter to keep default location:\${RESET}
-	  '';
-	restore = ''
-	    \${BOLD}\${WHITE}Where do you have your dotfiles?
-	'';
+      Press enter to keep default location:\${RESET}
+    '';
+    restore = ''
+      \${BOLD}\${WHITE}Where do you have your dotfiles?
+    '';
   };
   script = with import ./utilities/log_helpers.nix; { type }: ''
     echo -e "${messages."${type}"}" && read -p "" USER_DOTFILES_PATH
