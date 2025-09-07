61332ccb99879e559291dbe2bffbd5513c8b8c01
Gilberto Olivas <olivasgilberto@gmail.com>
2025-09-06 21:18:47 -0600

    core update

--- dotfiles_template/env.nix
+++ dotfiles_template/env.nix
@@ -1,15 +1,6 @@
 {
-  # Default to the current user if available, otherwise fall back to "root".
-  user = builtins.getEnv "USER" or "root";
-
-  # Default to the current home directory if available, otherwise fall back to "/root".
-  home = builtins.getEnv "HOME" or "/root";
-
-  # Default dotfiles directory path.  If the environment variable is not set,
-  # use a sensible default under the user's home directory.
-  dotfilesDirectory = builtins.getEnv "DOTLYX_HOME_PATH" or "${home}/.dotfiles";
-
-  # Default dotlyx home path.  If the environment variable is not set,
-  # use a sensible default under the user's home directory.
-  dotlyxDirectory = builtins.getEnv "DOTLYX_HOME_PATH" or "${home}/.dotlyx";
+  user = "XXX_USER_NAME_XXX";
+  home = "XXX_USER_HOME_XXX";
+  dotfilesDirectory = "XXX_USER_DOTFILES_PATH_XXX";
+  dotlyxDirectory = "XXX_DOTLYX_HOME_PATH_XXX";
 }
